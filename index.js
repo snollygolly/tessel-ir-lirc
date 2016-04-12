67a25cc02b63b636101de7e086d5c9d67c47f8ae
snollygolly <snollygolly@gmail.com>
2016-04-12 15:53:23 -0500

    added listing function

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const rp = require("request-promise");
 const cheerio = require("cheerio");
 const co = require("co");
 
-const LIRC_URL = "http://lirc.sourceforge.net/remotes/";
+const LIRC_URL = "http://lirc.sourceforge.net/remotes";
 
 function* getOEMLink(oem) {
 	// first get the page
@@ -16,17 +16,44 @@ function* getOEMLink(oem) {
 	// set the pattern [ "oem/" ]
 	const oemPattern = new RegExp(`"${filteredOEM}\\/"`, "g");
 	// check if it exists in the page
-	console.log(oemPattern);
 	if (oemPattern.test(rawPage) === true) {
 		console.log(`The OEM ${filteredOEM} is in the page!`);
-	} else {
-		console.log(`The OEM ${filteredOEM} is not in the page!`);
+		return filteredOEM;
 	}
-	return null;
+	console.log(`The OEM ${filteredOEM} is not in the page!`);
+	return false;
+}
+
+function* getAllRemotes(oem) {
+	// init the var
+	const remotes = [];
+	// first get the page
+	const rawPage = yield rp(`${LIRC_URL}/${oem}/`);
+	const $ = cheerio.load(rawPage);
+	$("tr").each((i, elem) => {
+		// this is each row, we need the second column
+		const tableLength = $(elem).children("td").length;
+		if (tableLength > 0) {
+			const remoteName = $(elem).children("td").eq(1).children("a").prop("href");
+			remotes.push(remoteName);
+		}
+	});
+	console.log(`We found ${remotes.length} remotes for this OEM`);
+	return remotes;
 }
 
 co(function* send() {
-	const result = yield getOEMLink("sony");
+	const oem = "pioneer";
+	// first see if you can get the OEM
+	const result = yield getOEMLink(oem);
+	if (result === false) {
+		throw new Error("No OEM found");
+	}
+	// get all the remotes on that listing page
+	const listing = yield getAllRemotes(result);
+	if (listing.length === 0) {
+		throw new Error("No remotes found");
+	}
 }).catch(onerror);
 
 function onerror(err) {
